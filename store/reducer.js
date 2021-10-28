96b3940cd1bb3e66a6150dd3823c9f85bdbccc71
Calvin <zxw880507@gmail.com>
2021-10-28 14:02:25 -0600

    add more reducers

--- store/reducer.js
+++ store/reducer.js
@@ -2,9 +2,13 @@ import { combineReducers } from "redux";
 import productReducer from "./features/productSlice";
 import checkboxReducer from "./features/checkboxSlice";
 import formReducer from "./features/formSlice";
+import modeReducer from "./features/modeSlice";
+import demandReducer from "./features/demandSlice";
 
 export default combineReducers({
   productState: productReducer,
   checkboxState: checkboxReducer,
   formState: formReducer,
+  modeState: modeReducer,
+  demandState: demandReducer,
 });
