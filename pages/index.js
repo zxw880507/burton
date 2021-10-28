573e35e8672807ba84a92628ee500c01a5520321
Calvin <zxw880507@gmail.com>
2021-10-28 14:01:53 -0600

    add more features

--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,41 @@
 import { useEffect } from "react";
 import { Container, Grid, Typography, Button } from "@mui/material";
+import LoadingOnDemand from "../components/LoadingOnDemand";
 import { useDispatch, useSelector } from "react-redux";
 import { getProductById } from "../store/features/productSlice";
 import { checkboxState, getCheckbox } from "../store/features/checkboxSlice";
-import CheckBoxGroup from "../components/checkBoxGroup";
+import CheckBoxGroup from "../components/CheckBoxGroup";
 import { setFormState, formState } from "../store/features/formSlice";
+import { toggleMode, modeState } from "../store/features/modeSlice";
 
 function HomePage() {
   const dispatch = useDispatch();
   const { checkbox, status, error } = useSelector(checkboxState);
   const { form } = useSelector(formState);
+  const mode = useSelector(modeState);
 
   useEffect(() => {
-    dispatch(getProductById("W22-219571")).then(() =>
+    dispatch(getProductById("W22-228201")).then(() =>
       dispatch(getCheckbox())
         .unwrap()
         .then(() => dispatch(setFormState()))
     );
   }, []);
 
+  if (mode) {
+    return <LoadingOnDemand />;
+  }
   return (
-    <Container maxWidth="sm">
+    <Container
+      maxWidth="sm"
+      sx={{ display: "flex", alignItems: "center", justifyContent: "center" }}
+    >
       {status === "succeeded" && (
         <Grid>
           {Object.keys(checkbox).map((key, index) => (
             <CheckBoxGroup key={index} label={key} values={checkbox[key]} />
           ))}
-          <Button
-            variant="contained"
-            onClick={() => console.log("this is form:", form)}
-          >
+          <Button variant="contained" onClick={() => dispatch(toggleMode())}>
             submit
           </Button>
         </Grid>
