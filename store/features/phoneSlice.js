c74fd3925a8deda683ce093de53b62d8ae69f9bc
Calvin <zxw880507@gmail.com>
2021-10-29 01:06:08 -0600

    create phone state

---
+++ store/features/phoneSlice.js
@@ -0,0 +1,56 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const initialState = {
+  phoneNumber: null,
+  status: "idle",
+  isSent: false,
+  message: null,
+};
+
+export const sendSMSMessage = createAsyncThunk(
+  "sendSMSMessage",
+  async (_, { getState, rejectWithValue }) => {
+    const { phoneNumber } = getState().phoneState;
+    const { demand } = getState().demandState;
+    try {
+      const res = await axios.post(`/api/sms/${phoneNumber}`, { demand });
+      return res.data;
+    } catch (error) {
+      if (!error.response) {
+        throw err;
+      }
+      return rejectWithValue(error.response.data);
+    }
+  }
+);
+
+const phoneSlice = createSlice({
+  name: "phone",
+  initialState,
+  reducers: {
+    getPhoneNumber(state, action) {
+      state.phoneNumber = action.payload;
+    },
+  },
+  extraReducers(builder) {
+    builder
+      .addCase(sendSMSMessage.pending, (state) => {
+        state.status = "sending";
+      })
+      .addCase(sendSMSMessage.fulfilled, (state, action) => {
+        state.status = "sent";
+        state.isSent = true;
+        state.message = action.payload;
+      })
+      .addCase(sendSMSMessage.rejected, (state, action) => {
+        state.status = "unsent";
+        state.isSent = false;
+        state.message = action.payload.errorMessage;
+      });
+  },
+});
+
+export default phoneSlice.reducer;
+export const { getPhoneNumber } = phoneSlice.actions;
+export const phoneState = (state) => state.phoneState;
