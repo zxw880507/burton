35e27fa25831145eb5a5a9846b51fe154dc7ae3b
Calvin <zxw880507@gmail.com>
2021-10-29 15:00:52 -0600

    fix bugs

--- pages/api/sms/[phoneNumber].js
+++ pages/api/sms/[phoneNumber].js
@@ -4,6 +4,7 @@ import twilio from "twilio";
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const twilioNumber = process.env.TWILIO_PHONE_NUMBER;
+console.log(accountSid, authToken);
 const client = twilio(accountSid, authToken);
 
 const handler = nc()
