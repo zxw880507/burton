5b5407808b897b6ed4813363684ae8e2e64a1c14
Calvin <zxw880507@gmail.com>
2021-10-29 01:05:24 -0600

    refactoring homepage

--- pages/index.js
+++ pages/index.js
@@ -1,47 +1,45 @@
-import { useEffect } from "react";
-import { Container, Grid, Typography, Button } from "@mui/material";
-import LoadingOnDemand from "../components/LoadingOnDemand";
-import { useDispatch, useSelector } from "react-redux";
-import { getProductById } from "../store/features/productSlice";
-import { checkboxState, getCheckbox } from "../store/features/checkboxSlice";
-import CheckBoxGroup from "../components/CheckBoxGroup";
-import { setFormState, formState } from "../store/features/formSlice";
-import { toggleMode, modeState } from "../store/features/modeSlice";
+import { useState } from "react";
+import { Container, Button, TextField } from "@mui/material";
+import { useRouter } from "next/router";
 
 function HomePage() {
-  const dispatch = useDispatch();
-  const { checkbox, status, error } = useSelector(checkboxState);
-  const { form } = useSelector(formState);
-  const mode = useSelector(modeState);
+  const router = useRouter();
+  const [input, setInput] = useState("");
+  
 
-  useEffect(() => {
-    dispatch(getProductById("W22-228201")).then(() =>
-      dispatch(getCheckbox())
-        .unwrap()
-        .then(() => dispatch(setFormState()))
-    );
-  }, []);
-
-  if (mode) {
-    return <LoadingOnDemand />;
-  }
   return (
     <Container
       maxWidth="sm"
-      sx={{ display: "flex", alignItems: "center", justifyContent: "center" }}
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        marginTop: "2em",
+      }}
     >
-      {status === "succeeded" && (
-        <Grid>
-          {Object.keys(checkbox).map((key, index) => (
-            <CheckBoxGroup key={index} label={key} values={checkbox[key]} />
-          ))}
-          <Button variant="contained" onClick={() => dispatch(toggleMode())}>
-            submit
-          </Button>
-        </Grid>
-      )}
-      {status === "loading" && <Typography variant="h1">LOADING</Typography>}
-      {status === "failed" && <Typography variant="h1">{error}</Typography>}
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        <TextField
+          label="product id"
+          variant="standard"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <Button
+          variant="outlined"
+          style={{ marginLeft: "1rem" }}
+          onClick={() => {
+            router.push(`/${input}`);
+          }}
+        >
+          Confirm
+        </Button>
+      </div>
     </Container>
   );
 }
