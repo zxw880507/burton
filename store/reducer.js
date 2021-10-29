3bd727a8fec736a7a35109afae8672f15662eb5b
Calvin <zxw880507@gmail.com>
2021-10-29 01:05:57 -0600

    import reducers

--- store/reducer.js
+++ store/reducer.js
@@ -4,11 +4,22 @@ import checkboxReducer from "./features/checkboxSlice";
 import formReducer from "./features/formSlice";
 import modeReducer from "./features/modeSlice";
 import demandReducer from "./features/demandSlice";
+import phoneReducer from "./features/phoneSlice";
 
-export default combineReducers({
+const appReducer = combineReducers({
   productState: productReducer,
   checkboxState: checkboxReducer,
   formState: formReducer,
   modeState: modeReducer,
   demandState: demandReducer,
+  phoneState: phoneReducer,
 });
+
+const rootReducer = (state, action) => {
+  if (action.type === "RESET") {
+    return appReducer(undefined, action);
+  }
+  return appReducer(state, action);
+};
+
+export default rootReducer;
